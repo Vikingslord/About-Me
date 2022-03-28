70d94e7e248e9a2bf1ffd9ddfd724d0856dbffd7
Vikingslord <ahsanulhaque1998@outlook.com>
2022-03-28 16:11:45 +0600

    2ndForm

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val doneBTN: Button = findViewById(R.id.done_button)
+        doneBTN.setOnClickListener {
+            changeName(it)
+        }
+    }
+
+    private fun changeName(view: View) {
+        val editText: EditText = findViewById(R.id.nicknameEdit)
+        val nicknameTextView: TextView = findViewById(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
     }
 }
