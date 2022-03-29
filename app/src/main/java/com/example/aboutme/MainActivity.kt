e358382b685f79ebdea8acab6431aeec907bdd0f
Vikingslord <ahsanulhaque1998@outlook.com>
2022-03-29 17:48:30 +0600

    updated

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,29 +2,33 @@ package com.example.aboutme
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Ahsanul")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val doneBTN: Button = findViewById(R.id.done_button)
-        doneBTN.setOnClickListener {
-            changeName(it)
+        binding.doneButton.setOnClickListener {
+            changeName()
         }
+        binding.myName = myName
     }
 
-    private fun changeName(view: View) {
-        val editText: EditText = findViewById(R.id.nicknameEdit)
-        val nicknameTextView: TextView = findViewById(R.id.nickname_text)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+    private fun changeName() {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+            binding.nicknameText.text = binding.nicknameEdit.text.toString()
+        }
     }
 }
